fbb76310fbade9096952c6d7d5ad8b94a0d3c9ce
Bartek Fabiszewski <github@fabiszewski.net>
2014-11-11 00:09:01 +0100

    fix: variable length value wrongly calculated when going backwards

--- src/buffer.c
+++ src/buffer.c
@@ -231,8 +231,9 @@ static uint32_t _buffer_get_varlen(MOBIBuffer *buf, size_t *len, const int direc
     uint32_t val = 0;
     uint8_t byte_count = 0;
     uint8_t byte;
-    uint8_t stop_flag = 0x80U;
-    uint8_t mask = 0x7fU;
+    uint8_t stop_flag = 0x80;
+    uint8_t mask = 0x7f;
+    uint32_t shift = 0;
     do {
         if (direction == 1) {
             if (buf->offset + 1 > buf->maxlen) {
@@ -241,6 +242,8 @@ static uint32_t _buffer_get_varlen(MOBIBuffer *buf, size_t *len, const int direc
                 return val;
             }
             byte = buf->data[buf->offset++];
+            val <<= 7;
+            val |= (byte & mask);
         } else {
             if (buf->offset < 1) {
                 debug_print("%s", "End of buffer\n");
@@ -248,9 +251,9 @@ static uint32_t _buffer_get_varlen(MOBIBuffer *buf, size_t *len, const int direc
                 return val;
             }
             byte = buf->data[buf->offset--];
-        }
-        val <<= 7;
-        val |= (byte & mask);
+            val = val | (uint32_t)(byte & mask) << shift;
+            shift += 7;
+        }        
         (*len)++;
         byte_count++;
     } while (!(byte & stop_flag) && (byte_count < 4));
